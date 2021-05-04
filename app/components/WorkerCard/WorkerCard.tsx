15a39c4e0cf6e4058ae4aa77277182f92c014735
liamdebellada <liam.debell@ada.ac.uk>
2021-05-04 12:35:18 +0100

    Removed sqlite3 from dependencies

--- app/components/WorkerCard/WorkerCard.tsx
+++ app/components/WorkerCard/WorkerCard.tsx
@@ -3,7 +3,7 @@ import Router from 'next/router';
 import {Worker} from '../../interfaces/worker';
 
 type Props = {
-    worker: Worker
+    worker?: Worker
 }
 
 export default function WorkerCard(props: Props){
@@ -11,13 +11,13 @@ export default function WorkerCard(props: Props){
     return (
         <div className={`${styles.iChallenge} ${styles.myChallenge}`} onClick={() => {Router.push(`/workers/1`)}}>
             <div className={styles.profileInfoContainer}>
-                <img className={styles.profilePicture} src={props.worker.picture} />
+                <img className={styles.profilePicture} src={props.worker?.picture} />
                 <div className={styles.profileText}>
-                    <div className={styles.profileName}>{props.worker.name}</div>
+                    <div className={styles.profileName}>{props.worker?.name}</div>
                     
                     <div className={styles.starContainer}>
                         <img className={styles.star} src="/star.svg"/>
-                        <div className={styles.ratingText}>{props.worker.rating}</div>
+                        <div className={styles.ratingText}>{props.worker?.rating}</div>
                         <span className={styles.circle}></span>
                     </div>
                 </div>
@@ -26,12 +26,12 @@ export default function WorkerCard(props: Props){
             <div className={styles.jobSection}>
                 <div className={styles.jobRow}>
                     <img className={styles.workTypeImage} src="/pizza.svg"/>
-                    <div className={styles.jobDetails}>{props.worker.jobs[0]}</div>
+                    <div className={styles.jobDetails}>{props.worker?.jobs[0]}</div>
                 </div>
 
                 <div className={styles.jobRow}>
                     <img className={styles.workTypeImage} src="/collection.svg"/>
-                    <div className={styles.jobDetails}>{props.worker.jobs[1]}</div>
+                    <div className={styles.jobDetails}>{props.worker?.jobs[1]}</div>
                 </div>
             </div>
 
