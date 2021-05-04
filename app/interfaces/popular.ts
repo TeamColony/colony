83a4702449db3cde3a06af14b800aef0388e09a6
Bruno Silva <bruno.silva@ada.ac.uk>
2021-05-04 09:17:51 +0100

    Types for home screen, dynamic content

---
+++ app/interfaces/popular.ts
@@ -0,0 +1,26 @@
+export type Popular = {
+    image: string
+    name: string
+  }
+  
+let popularJobs: Array<Popular> = [
+    {
+      image: "/pizza.svg",
+      name: "Takeaway"
+    },
+    {
+      image: "/drycleaning.svg",
+      name: "Dry Cleaning",
+    },
+    {
+        image: "/collection.svg",
+        name: "Collection",
+    },
+    {
+        image: "/babysitting.svg",
+        name: "Babysitting",
+    }
+  ]
+
+export default popularJobs;
+  
---
+++ app/interfaces/worker.ts
@@ -0,0 +1,31 @@
+
+export type Worker = {
+    name: string
+    rating: number
+    picture: string
+    jobs: Array<String>
+  }
+  
+let nearYou: Array<Worker> = [
+    {
+      name: "James McDaniel",
+      rating: 4.4,
+      picture: "/profile_pics/grey.png",
+      jobs: ["Food Delivery", "Collection"]
+    },
+    {
+      name: "Tina Gotschi",
+      rating: 5.0,
+      picture: "/profile_pics/tina.png",
+      jobs: ["Food Delivery", "Collection", "Dry Cleaning", "Babysitting", "Teaching"]
+    },
+    {
+      name: "Clide Calzone",
+      rating: 5.0,
+      picture: "/profile_pics/stark.png",
+      jobs: ["Food Delivery", "Collection", "Dry Cleaning", "Babysitting", "Teaching"]
+    },
+  ]
+
+export default nearYou;
+  
