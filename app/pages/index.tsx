90389dd2f326d1aaf814158e5630743d93c6dfa2
Bruno Silva <bruno.silva@ada.ac.uk>
2021-05-07 19:42:48 +0100

    Dynamic popular jobs from database

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -19,12 +19,18 @@ import userMessages from '../interfaces/messages';
 import { useQuery, gql } from '@apollo/client';
 const LeafletMap = dynamic(() => import("../components/Map"), { ssr: false });
 
+export type job = {
+    name: string,
+    image: string
+}
+
 export default function IndexPage () {
 
   const jobs = gql`
      {
        findAllJobs{
          name
+         image
        }
      }
   `;
@@ -32,63 +38,67 @@ export default function IndexPage () {
 
   const { loading, error, data } = useQuery(jobs);
 
-  return (
-    console.log(data),
+  if(loading){
+      return (<>Loading</>)
+  }
+  
+  if(data){
 
-    <div className={styles.indexBody}>
-      <div className={styles.mapParent}>
-        <div className={styles.popular}>
-          <div className={styles.jobHeader}>
-            <span className={styles.popularTitle}>Popular Jobs</span>
-            <span className={`${styles.popularAll} ${styles.unselectable}`}>View all</span>
+    return (
+      <div className={styles.indexBody}>
+        <div className={styles.mapParent}>
+          <div className={styles.popular}>
+            <div className={styles.jobHeader}>
+              <span className={styles.popularTitle}>Popular Jobs</span>
+              <span className={`${styles.popularAll} ${styles.unselectable}`}>View all</span>
+            </div>
+            <div className={styles.jobList}>
+                {data.findAllJobs.map((job: job) => (
+                    <div onClick={() => {Router.push(`/categories/1`)}} className={styles.popularJob}>
+                      <img className={styles.popularPicture} src={job.image} />
+                      <span>{job.name}</span>
+                    </div>
+                ))}
+            </div>
           </div>
-          <div className={styles.jobList}>
-              {popularJobs.map((job) => (
-                  <div onClick={() => {Router.push(`/categories/1`)}} className={styles.popularJob}>
-                    <img className={styles.popularPicture} src={job.image} />
-                    <span>{job.name}</span>
-                  </div>
-              ))}
+          <LeafletMap />
+        </div>
+
+        <div className={styles.messageHeader}>
+          <div className={styles.headerLeft}>
+            <span className={`material-icons ${styles.navIcon}`}>chat</span>
+            <div className={styles.navText}>Messages</div>
+          </div>
+        
+          <span onClick={() => {Router.push(`/messages`)}}
+              className={`${styles.popularAll} ${styles.unselectable}`}>View all</span>
+        </div>
+
+        <MessageCard message={userMessages[0]}></MessageCard>
+
+        <div className={styles.nearHeader}>
+          <div className={styles.headerLeft}>
+            <span className={`material-icons ${styles.navIcon}`}>location_pin</span>
+            <div className={styles.navText}>Near You</div>
           </div>
         </div>
-        <LeafletMap />
+
+        <Splide className={styles.splideComponent}
+          options={{
+            rewind: true,
+            gap: '2rem',
+            pagination: false,
+            autoHeight: true,
+            padding: "60px",
+            fixedWidth: "285px",
+          }}>
+
+          {nearYou.map((worker, i) => (
+              <SplideSlide className={`${i == 1 && styles.firstSplide}`}>
+                  <WorkerCard worker={worker}></WorkerCard>
+              </SplideSlide>                    
+          ))}
+        </Splide>
       </div>
-
-      <div className={styles.messageHeader}>
-        <div className={styles.headerLeft}>
-           <span className={`material-icons ${styles.navIcon}`}>chat</span>
-           <div className={styles.navText}>Messages</div>
-        </div>
-       
-        <span onClick={() => {Router.push(`/messages`)}}
-            className={`${styles.popularAll} ${styles.unselectable}`}>View all</span>
-      </div>
-
-      <MessageCard message={userMessages[0]}></MessageCard>
-
-      <div className={styles.nearHeader}>
-        <div className={styles.headerLeft}>
-          <span className={`material-icons ${styles.navIcon}`}>location_pin</span>
-          <div className={styles.navText}>Near You</div>
-        </div>
-      </div>
-
-      <Splide className={styles.splideComponent}
-        options={{
-          rewind: true,
-          gap: '2rem',
-          pagination: false,
-          autoHeight: true,
-          padding: "60px",
-          fixedWidth: "285px",
-        }}>
-
-        {nearYou.map((worker, i) => (
-            <SplideSlide className={`${i == 1 && styles.firstSplide}`}>
-                <WorkerCard worker={worker}></WorkerCard>
-            </SplideSlide>                    
-        ))}
-      </Splide>
-    </div>
-  )
+    )}
 }
