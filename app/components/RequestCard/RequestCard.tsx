8bc4fb7dbc56e0d15ebb223a779c9b0526ec085c
Bruno Silva <bruno.silva@ada.ac.uk>
2021-05-20 23:27:13 +0100

    Improved request styling, added simple job type menu

--- app/components/RequestCard/RequestCard.tsx
+++ app/components/RequestCard/RequestCard.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './request.module.css';
 import { gql, useMutation } from '@apollo/client';
 import { ifError } from 'assert';
 
-export default function RequestCard(props: any){
+export default function RequestCard(props: any) {
 
     const updateRequestStatus = gql`
         mutation statusMutation($input: updateRequest!){
@@ -13,13 +13,14 @@ export default function RequestCard(props: any){
 
     const [update] = useMutation(updateRequestStatus);
     const [shown, setShown] = useState(true);
+    const [currentStatus, setStatus] = useState(props.request.status);
 
-    if(props.request == null){
+    if (props.request == null) {
         return (<></>)
     }
 
     const updateStatus = (id: any, status: Number, key: Number) => {
-        
+
         update({
             variables: {
                 "input": {
@@ -29,66 +30,74 @@ export default function RequestCard(props: any){
             }
         }).then(data => {
             console.log(data.data.updateStatus);
-            if(data.data.updateStatus){
-                if(status == 2){
-                    setShown(false);
+            if (data.data.updateStatus) {
+                if (status == 2) {
+                    //setShown(false);
                 }
             }
+            setStatus(status);
         })
 
     }
 
-    return(
+    return (
         <div>
             {shown == true ? (
-                <div className={styles.requestCard}>
+                <div className={styles.requestCard} style={{ border: currentStatus == 1 ? 'solid #73C2A6 3px' : currentStatus == 2 ? '2px solid #EF8080' : '2px solid #8d86c9' }}>
                     <div className={styles.requestHeader}>
                         <div className={styles.leftHeader}>
                             <img src={props.request.user.image}></img>
                             <span>{props.request.user.name}</span>
                         </div>
-        
+
                         <div className={styles.rightHeader}>
-                            <span>1</span>
+                            <span>{props.request.status}</span>
                             <span className="material-icons">waving_hand</span>
                         </div>
-        
+
                     </div>
-        
+
                     <span className={styles.requestText}>{props.request.request}</span>
-                    
-                    <hr className={styles.separator}></hr>
-        
+
+                    <hr style={{ borderTop: currentStatus == 1 ? '2px solid #73C2A6' : currentStatus == 2 ? '2px solid #EF8080' : '2px solid #9e98d0'}} className={styles.separator}></hr>
+
                     <div className={styles.distance}>
                         <span className="material-icons">location_pin</span>
                         <span>{props.request.address}</span>
                     </div>
-        
+
                     <div className={styles.requestBottom}>
                         <div className={styles.leftBottom}>
-                            <div onClick={()=> updateStatus(props.request._id, 1, props.key)} 
-                                className={styles.thumbsUp}>
+                            {currentStatus == 1 ? (
+                                <div className={styles.upStatus}>
                                     <span className="material-icons">thumb_up</span>
-                            </div>
-        
-                            <div onClick={()=> updateStatus(props.request._id, 2, props.key)} className={styles.thumbsDown}>
-                                <span className="material-icons">thumb_down</span>
-                            </div>
+                                </div>
+                            ) : (<div onClick={() => updateStatus(props.request._id, 1, props.key)}
+                                className={styles.thumbsUp}>
+                                <span className="material-icons">thumb_up</span>
+                            </div>)}
+
+                                <div onClick={() => updateStatus(props.request._id, 2, props.key)} className={styles.thumbsDown}>
+                                    <span className="material-icons">thumb_down</span>
+                                </div>
+                            
+                        
+
                         </div>
-        
+
                         <div className={styles.rightBottom}>
                             <img src={props.request.job.image}></img>
                             <span>{props.request.job.name}</span>
                         </div>
-        
+
                     </div>
                 </div>
-            ): (
-                null
-            )}
+            ) : (
+                    null
+                )}
         </div>
-        
-        
+
+
 
     )
 }
